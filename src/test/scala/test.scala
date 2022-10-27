dda63ba753b541d7d0193fde0bcd641b8ad9385a
Alextem <temirbulatow1@gmail.com>
2022-10-27 14:18:29 +0200

    test class

---
+++ src/test/scala/test.scala
@@ -0,0 +1,31 @@
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.matchers.should.Matchers._
+
+class test extends AnyWordSpec {
+
+    "CandyCrush" should {
+        "have a horizontal String of form '+----+'" in {
+            horizontal() should be("+----+")
+        }
+
+        "have a scalable bar" in {
+            horizontal(2) should be("+----+----+")
+            horizontal(3) should be("+----+----+----+")
+            horizontal(4) should be("+----+----+----+----+")
+        }
+
+        "have a String of form '| " + "uu '" in {
+            printuu() should be("| " + "uu ")
+        }
+
+        "have a String of form '| " + "oo '" in {
+            printoo() should be("| " + "oo ")
+        }
+
+        "have a String of form '| " + "zz '" in {
+            printzz() should be("| " + "zz ")
+        }
+
+    }
+
+}
