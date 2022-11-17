cfa34d7bea970a22988a5a463f5240107124b287
Yazanski <116159892+Yazanski@users.noreply.github.com>
2022-11-17 23:48:14 +0100

    Delete main/scala directory

--- main/scala/de/htwg/model/Stone.scala
+++
@@ -1,6 +0,0 @@
-package de.htwg.model
-
-class Stone:
-  def printuu(): String = Console.BLUE + "uu" + Console.WHITE
-  def printoo(): String = Console.MAGENTA + "oo" + Console.WHITE
-  def printzz(): String = Console.GREEN + "zz" + Console.WHITE
